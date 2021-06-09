910a1faa08c84e113cb81921c0e47f12292e1898
Melanie2010 <65156750+Melanie2010@users.noreply.github.com>
2021-06-09 10:24:00 +0200

    Update index.php

--- index.php
+++ index.php
@@ -5,14 +5,12 @@ $objetPdo = new PDO('mysql:host=localhost;dbname=client_ligue','root','');
 $objetPdo->exec("SET CHARACTER SET utf8");
 
 //requete d affichage
-
 $pdoStat = $objetPdo->prepare('SELECT * FROM clients ');
 
 //executer la requete
-
 $executeIsOk = $pdoStat->execute();
-//recuperation des resulats
 
+//recuperation des resulats
 $clients = $pdoStat->fetchAll();
 
 ?>
@@ -26,39 +24,37 @@ $clients = $pdoStat->fetchAll();
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="./css/style.css">
     <title>Maison des Ligues</title>
-
 </head>
 
 <body>
-<h1><img class="icone_paramètre" src="./asset/paramètre.png" alt="coney"> Maison des Ligues : Interface admin </h1>
-   
+
+    <h1 style="padding-top:10px; border: 3px solid #A0A0A0; text-align: center;background: black;"> &#9965; Maison des Ligues : Interface admin</h1>
+ 
     <table id="liste_client">
             <thead>
             <tr>
-                <th >Identifiant</th>
-                <th >Nom</th>
-                <th >Prenom</th>
-                <th >Age</th>
-                <th >Adresse Mail</th>
+                <th>Identifiant</th>
+                <th>Nom</th>
+                <th>Prénom</th>
+                <th>Age</th>
+                <th>Adresse Mail</th>
             </tr>
             </thead>
 
          <?php foreach($clients as $clients):
          {
          ?>
-
          <tbody>
             
-                 <td>
-                    <a href="supprimer.php?numclients=<?= $clients['Id_client']?>"> Supprimer</a>
+                 <td style="text-align: center">
+                    <a href="modif.php?numclients=<?= $clients['Id_client']?>" style="text-decoration:none">&#9998; Modifer</a>
                     ou
-                    <a href="modif.php?numclients=<?= $clients['Id_client']?>">Modifer</a>
+                    <a href="modif.php?numclients=<?= $clients['Id_client']?>" style="text-decoration:none">&#9746; Supprimer</a>
                  </td>
                  <td><?php echo $clients['nom'];?></td>
                  <td><?php echo $clients['prenom'];?></td>
-                 <td><?php echo $clients['age'];?></td>
-                 <td><?php echo $clients['email'];?></td>
-                
+                 <td style="text-align: center"><?php echo $clients['age'];?></td>
+                 <td><?php echo $clients['email'];?></td>   
         </tbody>
         <?php  
         }
@@ -66,11 +62,8 @@ $clients = $pdoStat->fetchAll();
         ?>
        
     </table>
-    </br>
+    </br></br>
     <td><a class="boutton_ajout" href="ajout.php" style="text-decoration:none" >+ Ajouter un client</a></td>
-    
         
 </body>
 </html>
-
-           
